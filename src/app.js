6df7f8c3a1e71ba23402bfac819c603b67252001
drakarisbelen <kari.belen@gmail.com>
2021-01-04 20:36:31 -0300

    Agrego Procfile y PORT

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
 
-app.listen(3000,() =>{
+app.listen(process.env.PORT || 3000,() =>{
     console.log("--------------------------------------------------------------------------------------");
     console.log("-------------------------------SERVIDOR FUNCIONANDO-----------------------------------");
     console.log("--------------------------------------------------------------------------------------");
